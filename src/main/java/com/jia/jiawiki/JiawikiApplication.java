7fe3cc1d08d0b2df0f7a1423ed7951dd672137dc
sognshenglinsong_i <songshenglinsong_i@didiglobal.com>
2022-05-16 22:08:41 +0800

    2-2 init

---
+++ src/main/java/com/jia/jiawiki/JiawikiApplication.java
@@ -0,0 +1,13 @@
+package com.jia.jiawiki;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JiawikiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JiawikiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/jia/jiawiki/JiawikiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jia.jiawiki;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JiawikiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
