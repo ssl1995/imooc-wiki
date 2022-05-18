76e283b9de6735a053a73878d71737de5a4ab80e
sognshenglinsong_i <songshenglinsong_i@didiglobal.com>
2022-05-18 21:46:52 +0800

    4-4 制作the-header组件

--- web/src/views/Home.vue
+++ web/src/views/Home.vue
@@ -11,7 +11,7 @@
           <template #title>
               <span>
                 <user-outlined/>
-                subnav 11111
+                subnav 2222
               </span>
           </template>
           <a-menu-item key="1">option1</a-menu-item>
