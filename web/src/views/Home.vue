fcc567c71567d463e07e41681f0c8adca577c4df
sognshenglinsong_i <songshenglinsong_i@didiglobal.com>
2022-05-18 21:25:17 +0800

    4-2 网站首页开发

--- web/src/views/Home.vue
+++ web/src/views/Home.vue
@@ -1,5 +1,8 @@
 <template>
   <div class="home">
+
+    <a-button type="primary" danger ghost>Danger</a-button>
+
     <img alt="Vue logo" src="../assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
   </div>
