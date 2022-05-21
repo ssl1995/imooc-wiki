e1d68259e906ab58b0ee86aa8f573848dc8e2345
sognshenglinsong_i <songshenglinsong_i@didiglobal.com>
2022-05-21 20:47:47 +0800

    5-5 vue cli多环境配置：dev和prod

--- web/src/main.ts
+++ web/src/main.ts
@@ -10,8 +10,11 @@ import * as Icons from '@ant-design/icons-vue';
 const app = createApp(App)
 app.use(store).use(router).use(Antd).mount('#app')
 
-// 引用全部图标库
+/* 引用全部图标库 */
 const icons: any = Icons;
 for (const i in icons) {
     app.component(i, icons[i])
 }
+
+console.log("环境11：" + process.env.NODE_ENV)
+console.log("服务端：" + process.env.VUE_APP_SERVER)
