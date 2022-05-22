09cfba5d4828307680b8b6d2d10aeee8a8eb04fe
sognshenglinsong_i <songshenglinsong_i@didiglobal.com>
2022-05-22 08:45:44 +0800

    6-2 增加PageHelper

--- src/main/java/com/jiawa/wiki/service/impl/EbookServiceImpl.java
+++ src/main/java/com/jiawa/wiki/service/impl/EbookServiceImpl.java
@@ -1,5 +1,7 @@
 package com.jiawa.wiki.service.impl;
 
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
 import com.jiawa.wiki.domain.Ebook;
 import com.jiawa.wiki.domain.EbookExample;
 import com.jiawa.wiki.mapper.EbookMapper;
@@ -7,6 +9,8 @@ import com.jiawa.wiki.req.EbookReq;
 import com.jiawa.wiki.resp.EbookResp;
 import com.jiawa.wiki.service.EbookService;
 import com.jiawa.wiki.utils.CopyUtil;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.util.ObjectUtils;
@@ -21,6 +25,8 @@ import java.util.List;
 @Service
 public class EbookServiceImpl implements EbookService {
 
+    private static final Logger LOG = LoggerFactory.getLogger(EbookServiceImpl.class);
+
     @Autowired
     private EbookMapper ebookMapper;
 
@@ -32,8 +38,13 @@ public class EbookServiceImpl implements EbookService {
         if (!ObjectUtils.isEmpty(req.getName())) {
             criteria.andNameLike("%" + req.getName() + "%");
         }
-
+        // 使用PageHelper
+        PageHelper.startPage(1, 3);
         List<Ebook> ebookList = ebookMapper.selectByExample(ebookExample);
+
+        PageInfo<Ebook> pageInfo = new PageInfo<>(ebookList);
+        LOG.info("总行数：{}", pageInfo.getTotal());
+
         return CopyUtil.copyList(ebookList, EbookResp.class);
     }
 }
