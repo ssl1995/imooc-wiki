7e707920d463a5d256aa1e87de34dfbbd5f1cb75
sognshenglinsong_i <songshenglinsong_i@didiglobal.com>
2022-05-23 21:58:10 +0800

    7-8 首页左边栏分页展示菜单

--- web/src/views/home.vue
+++ web/src/views/home.vue
@@ -2,46 +2,24 @@
   <a-layout>
     <a-layout-sider width="200" style="background: #fff">
       <a-menu
-          v-model:selectedKeys="selectedKeys2"
-          v-model:openKeys="openKeys"
           mode="inline"
           :style="{ height: '100%', borderRight: 0 }"
+          @click="handleClick"
       >
-        <a-sub-menu key="sub1">
-          <template #title>
-              <span>
-                <user-outlined/>
-                subnav 2222
-              </span>
+        <a-menu-item key="welcome">
+          <router-link :to="'/'">
+            <MailOutlined/>
+            <span>欢迎</span>
+          </router-link>
+        </a-menu-item>
+        <a-sub-menu v-for="item in level1" :key="item.id">
+          <template v-slot:title>
+            <span><user-outlined/>{{ item.name }}</span>
           </template>
-          <a-menu-item key="1">option1</a-menu-item>
-          <a-menu-item key="2">option2</a-menu-item>
-          <a-menu-item key="3">option3</a-menu-item>
-          <a-menu-item key="4">option4</a-menu-item>
-        </a-sub-menu>
-        <a-sub-menu key="sub2">
-          <template #title>
-              <span>
-                <laptop-outlined/>
-                subnav 2
-              </span>
-          </template>
-          <a-menu-item key="5">option5</a-menu-item>
-          <a-menu-item key="6">option6</a-menu-item>
-          <a-menu-item key="7">option7</a-menu-item>
-          <a-menu-item key="8">option8</a-menu-item>
-        </a-sub-menu>
-        <a-sub-menu key="sub3">
-          <template #title>
-              <span>
-                <notification-outlined/>
-                subnav 3
-              </span>
-          </template>
-          <a-menu-item key="9">option9</a-menu-item>
-          <a-menu-item key="10">option10</a-menu-item>
-          <a-menu-item key="11">option11</a-menu-item>
-          <a-menu-item key="12">option12</a-menu-item>
+          <a-menu-item v-for="child in item.children" :key="child.id">
+            <MailOutlined/>
+            <span>{{ child.name }}</span>
+          </a-menu-item>
         </a-sub-menu>
       </a-menu>
     </a-layout-sider>
@@ -78,30 +56,42 @@
 <script lang="ts">
 import {defineComponent, onMounted, ref, reactive, toRef} from 'vue';
 import axios from 'axios'
+import {Tool} from "@/util/Tool";
+import {message} from "ant-design-vue";
 
 export default defineComponent({
   name: 'Home',
   setup() {
     // 声明响应式数据
     const ebooke1 = ref();
-    // const ebooke2 = reactive({"books": []});
 
+    const level1 = ref();
+    let categorys: any;
+    /**
+     * 查询所有分类
+     **/
+    const handleQueryCategory = () => {
+      axios.get("/category/all").then((response) => {
+        const data = response.data;
+        if (data.success) {
+          categorys = data.content;
+          console.log("原始数组：", categorys);
 
-    // const listData: Record<string, string>[] = [];
-    //
-    // for (let i = 0; i < 23; i++) {
-    //   listData.push({
-    //     href: 'https://www.antdv.com/',
-    //     title: `ant design vue part ${i}`,
-    //     avatar: 'https://joeschmoe.io/api/v1/random',
-    //     description:
-    //         'Ant Design, a design language for background applications, is refined by Ant UED Team.',
-    //     content:
-    //         'We supply a series of design principles, practical patterns and high quality design resources (Sketch and Axure), to help people create their product prototypes beautifully and efficiently.',
-    //   });
-    // }
+          level1.value = [];
+          level1.value = Tool.array2Tree(categorys, 0);
+          console.log("树形结构：", level1.value);
+        } else {
+          message.error(data.message);
+        }
+      });
+    };
+
+    const handleClick = () => {
+      console.log("menu click")
+    };
 
     onMounted(() => {
+      handleQueryCategory();
       // 发送http请求
       axios.get("/ebook/list", {
         params: {
@@ -132,6 +122,10 @@ export default defineComponent({
         {type: 'LikeOutlined', text: '156'},
         {type: 'MessageOutlined', text: '2'},
       ],
+
+      handleClick,
+      level1,
+
     }
 
 
