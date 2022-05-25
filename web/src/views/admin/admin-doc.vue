38bbae6ffcd6ca3414d9482d05371776e186f829
sognshenglinsong_i <songshenglinsong_i@didiglobal.com>
2022-05-25 15:17:16 +0800

    8-6 增加富文本wangedit编辑器

--- web/src/views/admin/admin-doc.vue
+++ web/src/views/admin/admin-doc.vue
@@ -73,6 +73,10 @@
       <a-form-item label="顺序">
         <a-input v-model:value="doc.sort"/>
       </a-form-item>
+
+      <a-form-item label="内容">
+        <div id="content"></div>
+      </a-form-item>
     </a-form>
   </a-modal>
 </template>
@@ -81,10 +85,10 @@
 import {defineComponent, onMounted, ref, createVNode} from 'vue';
 import axios from 'axios';
 import {message, Modal} from 'ant-design-vue';
-import {Tool} from "@/util/Tool";
 import {useRoute} from "vue-router";
 import ExclamationCircleOutlined from "@ant-design/icons-vue/ExclamationCircleOutlined";
-
+import E from 'wangeditor';
+import {Tool} from "@/util/Tool";
 
 export default defineComponent({
   name: 'AdminDoc',
@@ -167,6 +171,9 @@ export default defineComponent({
     const doc = ref({});
     const modalVisible = ref(false);
     const modalLoading = ref(false);
+    const editor = new E('#content');
+
+
     const handleModalOk = () => {
       modalLoading.value = true;
       axios.post("/doc/save", doc.value).then((response) => {
@@ -230,6 +237,10 @@ export default defineComponent({
 
       // 为选择树添加一个"无"
       treeSelectData.value.unshift({id: '0', name: '无'});
+
+      setTimeout(function () {
+        editor.create();
+      }, 100);
     };
 
     /**
@@ -246,6 +257,9 @@ export default defineComponent({
       // 为选择树添加一个"无"
       treeSelectData.value.unshift({id: '0', name: '无'});
 
+      setTimeout(function () {
+        editor.create();
+      }, 100);
     };
 
     const deleteIds: Array<string> = [];
