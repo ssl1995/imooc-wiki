9b9d9b9891bbe5735c87e7dd4a326e5097d2954d
songshenglin <872912679@qq.com>
2022-05-27 21:33:15 +0800

    9-17 用户名密码初试化：均为test

--- web/src/components/the-header.vue
+++ web/src/components/the-header.vue
@@ -78,7 +78,7 @@ export default defineComponent({
 
     const loginUser = ref({
       loginName: "test",
-      password: "test123"
+      password: "test"
     });
     const loginModalVisible = ref(false);
     const loginModalLoading = ref(false);
