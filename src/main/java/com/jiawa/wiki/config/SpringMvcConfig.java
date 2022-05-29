c2421bc83e016a40d2112abe8aeb55633129cb8e
songshenglin <872912679@qq.com>
2022-05-29 21:54:02 +0800

    12-17 电子书新增增加上传图片，本地测试

--- src/main/java/com/jiawa/wiki/config/SpringMvcConfig.java
+++ src/main/java/com/jiawa/wiki/config/SpringMvcConfig.java
@@ -29,9 +29,10 @@ public class SpringMvcConfig implements WebMvcConfigurer {
                         "/doc/all/**",
                         "/doc/vote/**",
                         "/doc/find-content/**",
-                        "/ebook-snapshot/**"
+                        "/ebook-snapshot/**",
+                        "/ebook/upload/avatar"
                 );
-        
+
         registry.addInterceptor(actionInterceptor)
                 .addPathPatterns(
                         "/*/save",
--- src/main/java/com/jiawa/wiki/controller/EbookController.java
+++ src/main/java/com/jiawa/wiki/controller/EbookController.java
@@ -6,10 +6,15 @@ import com.jiawa.wiki.req.EbookSaveReq;
 import com.jiawa.wiki.resp.CommonResp;
 import com.jiawa.wiki.resp.PageResp;
 import com.jiawa.wiki.service.EbookService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 import javax.validation.Valid;
+import java.io.File;
+import java.io.IOException;
 
 /**
  * @Author: SongShengLin
@@ -19,6 +24,9 @@ import javax.validation.Valid;
 @RestController
 @RequestMapping("ebook")
 public class EbookController {
+
+    private static final Logger LOG = LoggerFactory.getLogger(EbookController.class);
+
     @Autowired
     private EbookService ebookService;
 
@@ -44,6 +52,22 @@ public class EbookController {
     public CommonResp delete(@PathVariable Long id) {
         ebookService.delete(id);
         return new CommonResp<>();
+    }
+
+    @RequestMapping("/upload/avatar")
+    public CommonResp upload(@RequestParam MultipartFile avatar) throws IOException {
+        LOG.info("上传文件开始：{}", avatar);
+        LOG.info("文件名：{}", avatar.getOriginalFilename());
+        LOG.info("文件大小：{}", avatar.getSize());
+
+        // 保存文件到本地
+        String fileName = avatar.getOriginalFilename();
+        String fullPath = "/Users/songshenglin/Downloads/wiki/image/" + fileName;
+        File dest = new File(fullPath);
+        avatar.transferTo(dest);
+        LOG.info(dest.getAbsolutePath());
+
+        return new CommonResp();
     }
 
 
