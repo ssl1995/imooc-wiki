1063dbb7f4d03058f28eb2f1f7b984a93d470f18
songshenglin <872912679@qq.com>
2022-05-29 21:27:47 +0800

    12-12 优化：root不开放增删改,admin才有全部权限

--- src/main/java/com/jiawa/wiki/interceptor/ActionInterceptor.java
+++ src/main/java/com/jiawa/wiki/interceptor/ActionInterceptor.java
@@ -2,6 +2,8 @@ package com.jiawa.wiki.interceptor;
 
 import com.alibaba.fastjson.JSONObject;
 import com.jiawa.wiki.resp.CommonResp;
+import com.jiawa.wiki.resp.UserLoginResp;
+import com.jiawa.wiki.utils.LoginUserContext;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
@@ -24,7 +26,14 @@ public class ActionInterceptor implements HandlerInterceptor {
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws IOException {
         // OPTIONS请求不做校验,
         // 前后端分离的架构, 前端会发一个OPTIONS请求先做预检, 对预检请求不做校验
-        if("OPTIONS".equals(request.getMethod().toUpperCase())){
+        if ("OPTIONS".equals(request.getMethod().toUpperCase())) {
+            return true;
+        }
+
+        // 获取登录信息
+        UserLoginResp userLoginResp = LoginUserContext.getUser();
+        if ("admin".equals(userLoginResp.getLoginName())) {
+            // admin用户不拦截
             return true;
         }
 
