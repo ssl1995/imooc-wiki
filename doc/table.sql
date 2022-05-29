a17a74e9abde640ada7ecc4d254ea789a9b55fcc
songshenglin <872912679@qq.com>
2022-05-29 10:07:18 +0800

    11-1 增加快照表，三个报表sql

--- doc/table.sql
+++ doc/table.sql
@@ -111,16 +111,19 @@ create table `content`
 
 -- 用户表
 drop table if exists `user`;
-create table `user` (
-                        `id` bigint not null comment 'ID',
-                        `login_name` varchar(50) not null comment '登陆名',
-                        `name` varchar(50) comment '昵称',
-                        `password` char(32) not null comment '密码',
-                        primary key (`id`),
-                        unique key `login_name_unique` (`login_name`)
-) engine=innodb default charset=utf8mb4 comment='用户';
+create table `user`
+(
+    `id`         bigint      not null comment 'ID',
+    `login_name` varchar(50) not null comment '登陆名',
+    `name`       varchar(50) comment '昵称',
+    `password`   char(32)    not null comment '密码',
+    primary key (`id`),
+    unique key `login_name_unique` (`login_name`)
+) engine = innodb
+  default charset = utf8mb4 comment ='用户';
 
-insert into `user` (id, `login_name`, `name`, `password`) values (1, 'test', '测试', 'test');
+insert into `user` (id, `login_name`, `name`, `password`)
+values (1, 'test', '测试', 'test');
 
 # 更新文档数、阅读数、点赞数
 update ebook t1 ,(select ebook_id, count(1) as doc_count, sum(view_count) as view_count, sum(vote_count) as vote_count
@@ -130,6 +133,49 @@ set t1.doc_count  = t2.doc_count,
     t1.view_count=t2.view_count,
     t1.vote_count = t2.vote_count
 where t1.id = t2.ebook_id;
+
+-- 电子书快照表
+drop table if exists `ebook_snapshot`;
+create table `ebook_snapshot`
+(
+    `id`            bigint auto_increment not null comment 'id',
+    `ebook_id`      bigint                not null default 0 comment '电子书id',
+    `date`          date                  not null comment '快照日期',
+    `view_count`    int                   not null default 0 comment '阅读数',
+    `vote_count`    int                   not null default 0 comment '点赞数',
+    `view_increase` int                   not null default 0 comment '阅读增长',
+    `vote_increase` int                   not null default 0 comment '点赞增长',
+    primary key (`id`),
+    unique key `ebook_id_date_unique` (`ebook_id`, `date`)
+) engine = innodb
+  default charset = utf8mb4 comment ='电子书快照表';
+
+# 统计报表
+-- 1.快照表插入新数据,让id连续的写法
+insert into ebook_snapshot(ebook_id, date, view_count, vote_count, view_increase, vote_increase)
+select t1.id, curdate(), 0, 0, 0, 0
+from ebook t1
+where not exists(select 1 from ebook_snapshot t2 where t1.id = t2.ebook_id and t2.date = curdate());
+
+-- 2.更新快照表
+update ebook_snapshot t1,ebook t2
+set t1.view_count = t2.view_count,
+    t1.vote_count=t2.vote_count
+where t1.date = curdate()
+  and t1.ebook_id = t2.id;
+
+-- 3.获取昨天的数据
+# select ebook_id, view_count, vote_count
+# from ebook_snapshot
+# where date = date_sub(CURDATE(), interval 1 day);
+
+update ebook_snapshot t1 left join (select ebook_id, view_count, vote_count
+                                    from ebook_snapshot
+                                    where date = date_sub(CURDATE(), interval 1 day)) t2
+    on t1.ebook_id = t2.ebook_id
+set t1.view_increase = (t1.view_count - IFNULL(t2.view_count, 0)),
+    t1.vote_increase = (t1.vote_count - IFNULL(t2.vote_count, 0))
+where t1.date = curdate();
 
 
 
---
+++ src/main/java/com/jiawa/wiki/mapper/EbookSnapshotMapper.java
@@ -0,0 +1,30 @@
+package com.jiawa.wiki.mapper;
+
+import com.jiawa.wiki.domain.EbookSnapshot;
+import com.jiawa.wiki.domain.EbookSnapshotExample;
+import java.util.List;
+import org.apache.ibatis.annotations.Param;
+
+public interface EbookSnapshotMapper {
+    long countByExample(EbookSnapshotExample example);
+
+    int deleteByExample(EbookSnapshotExample example);
+
+    int deleteByPrimaryKey(Long id);
+
+    int insert(EbookSnapshot row);
+
+    int insertSelective(EbookSnapshot row);
+
+    List<EbookSnapshot> selectByExample(EbookSnapshotExample example);
+
+    EbookSnapshot selectByPrimaryKey(Long id);
+
+    int updateByExampleSelective(@Param("row") EbookSnapshot row, @Param("example") EbookSnapshotExample example);
+
+    int updateByExample(@Param("row") EbookSnapshot row, @Param("example") EbookSnapshotExample example);
+
+    int updateByPrimaryKeySelective(EbookSnapshot row);
+
+    int updateByPrimaryKey(EbookSnapshot row);
+}
