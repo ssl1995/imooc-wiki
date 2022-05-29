0d54d3210b14257a8e596d028ec59e46cc731baa
songshenglin <872912679@qq.com>
2022-05-29 11:49:19 +0800

    11-12 about.vue文案修改

--- web/src/views/about.vue
+++ web/src/views/about.vue
@@ -1,5 +1,7 @@
 <template>
-  <div class="about">
-    <h1>这是我的页面</h1>
-  </div>
+  <a-layout>
+    <a-layout-content :style="{ background: '#fff', padding: '24px', margin: 0, minHeight: '280px' }">
+      <h1>老宋，加油学习吧</h1>
+    </a-layout-content>
+  </a-layout>
 </template>
