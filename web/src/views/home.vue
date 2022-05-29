8e5402130759602463a73502a4a1a089d5256229
songshenglin <872912679@qq.com>
2022-05-29 10:36:15 +0800

    11-4 将欢迎表语提取成组件；上一个git增加快照的controller

--- web/src/views/home.vue
+++ web/src/views/home.vue
@@ -25,7 +25,7 @@
         :style="{ background: '#fff', padding: '24px', margin: 0, minHeight: '280px' }"
     >
       <div class="welcome" v-show="isShowWelcome">
-        <h1>欢迎使用甲蛙知识库</h1>
+        <the-welcome></the-welcome>
       </div>
       <a-list v-show="!isShowWelcome" item-layout="vertical" size="large" :grid="{ gutter: 20, column: 3 }"
               :data-source="ebooks">
@@ -67,10 +67,15 @@ import {defineComponent, onMounted, ref, reactive, toRef} from 'vue';
 import axios from 'axios';
 import {message} from 'ant-design-vue';
 import {Tool} from "@/util/Tool";
+import TheWelcome from '@/components/the-welcome.vue';
 
 
 export default defineComponent({
   name: 'Home',
+  components: {
+    TheWelcome
+  },
+
   setup() {
     const ebooks = ref();
     // const ebooks1 = reactive({books: []});
