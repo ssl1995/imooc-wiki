a50a9a825d4806c2763fa4c00dd7f76423c45f2e
songshenglin <872912679@qq.com>
2022-07-10 17:55:06 +0800

    关于我修改

--- web/src/components/the-header.vue
+++ web/src/components/the-header.vue
@@ -39,7 +39,7 @@
         <router-link to="/admin/category">分类管理</router-link>
       </a-menu-item>
       <a-menu-item key="/about">
-        <router-link to="/about">关于我们</router-link>
+        <router-link to="/about">关于我</router-link>
       </a-menu-item>
 
     </a-menu>
